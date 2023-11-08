ac61595dfa7f60541185af1a7948e6e921429423
Luciano Kohler da Silva <lucianokohlerdasilva@gmail.com>
2023-11-08 15:50:05 -0300

    arrumada de bugs geral e final

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,15 @@ function Reservar() {
   let sEscolhida = document.getElementById("sala").value;
   let hEscolhido = document.getElementById("horario").value;
   let isAvailable = true;
-  let check = document.getElementById("integrar").value;
+  let check = document.getElementById("integrar").checked;
+  console.log(check)
 
-if(sEscolhida > 203 || sEscolhida < 201){
-  window.alert("Sala não existe, apenas as salas 201, 202 e 203 estão disponíveis");
-  isAvailable = false;
-}
+  if (sEscolhida > 203 || sEscolhida < 201) {
+    window.alert(
+      "Sala não existe, apenas as salas 201, 202 e 203 estão disponíveis"
+    );
+    isAvailable = false;
+  }
   // Loop para verificar disponibilidade de sala e horário
   for (let i = 0; i < hOcupados.length; i++) {
     if (hOcupados[i][0] == hEscolhido && hOcupados[i][1] == sEscolhida) {
@@ -24,8 +27,13 @@ if(sEscolhida > 203 || sEscolhida < 201){
   }
 
   // Verifica se o for achou algo igual
-  if (isAvailable == true && check == 'on') {
-    window.alert("Horário Disponível, reserva Concluída e Integrada ao seu calendário!");
+  if (isAvailable == true && check) {
+    window.alert(
+      "Horário Disponível, reserva Concluída e Integrada ao seu calendário!"
+    );
+    hOcupados.push([hEscolhido, sEscolhida]);
+  } else if (isAvailable == true && check != "on") {
+    window.alert("Horário Disponível, reserva Concluída!");
     hOcupados.push([hEscolhido, sEscolhida]);
   } else {
     window.alert("Horário Indisponível. Tente outro horário.");
@@ -49,3 +57,23 @@ atualizaLista();
 
 ///////////////////////
 
+//integração do calendário
+// document
+//   .getElementById("add-to-calendar-button")
+//   .addEventListener("click", function () {
+//     const eventDetails = {
+//       text: "Nome do Evento",
+//       dates: "20231108T100000/20231108T120000", // Formato: AAAAMMDDTHHMMSS/AAAAMMDDTHHMMSS
+//       location: "Localização do Evento",
+//       details: "Descrição do Evento",
+//     };
+
+//     const encodedText = encodeURIComponent(eventDetails.text);
+//     const encodedDates = encodeURIComponent(eventDetails.dates);
+//     const encodedLocation = encodeURIComponent(eventDetails.location);
+//     const encodedDetails = encodeURIComponent(eventDetails.details);
+
+//     const googleCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodedText}&dates=${encodedDates}&location=${encodedLocation}&details=${encodedDetails}`;
+
+//     window.open(googleCalendarUrl, "_blank");
+//   });
